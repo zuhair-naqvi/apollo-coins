5e122b15bc0c680ed1afb655f0c5c1960afe0729
doc-manhattan <d.mhtn.init@gmail.com>
2020-07-28 22:12:04 +1000

    graph cms

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Cryptos from "./components/Cryptos";
+
+const client = new ApolloClient({
+  uri:
+    "https://api-eu-central-1.graphcms.com/v2/ckd5o75mz21fz01xu8s9c7nb1/master",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Crypto: {
+        fields: {
+          price: {
+            read(_, { readField }) {
+              return readField("polygonId");
+            },
+          },
+        },
+      },
+    },
+  }),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,8 +36,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </ApolloProvider>
       </SafeAreaProvider>
     );
   }
---
+++ components/Cryptos.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+
+import { useQuery, gql } from "@apollo/client";
+import { ActivityIndicator, StyleSheet, FlatList } from "react-native";
+import { View, Text } from "./Themed";
+
+import { ListItem } from "react-native-elements";
+
+const CRYPTOS = gql`
+  query getCryptos {
+    cryptos {
+      name
+      ticker
+      polygonId
+      logo {
+        id
+        url
+      }
+      price @client
+    }
+  }
+`;
+
+export default function Cryptos() {
+  const { loading, error, data } = useQuery(CRYPTOS);
+  if (error) {
+    return <Text>Error Fetching Data</Text>;
+  }
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  console.log(data);
+
+  const list = data?.cryptos?.map(
+    ({
+      name,
+      ticker,
+      price,
+      logo,
+    }: {
+      name: string;
+      ticker: string;
+      price: string;
+      logo: any;
+    }) => ({ name, ticker, price, avatar_url: logo.url })
+  );
+
+  const keyExtractor = (item, index) => index.toString();
+
+  const renderItem = ({ item }) => {
+    return (
+      <ListItem
+        title={item.name}
+        subtitle={item.ticker}
+        leftAvatar={{ source: { uri: item.avatar_url } }}
+        rightElement={<Text>{item.price}</Text>}
+        bottomDivider
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={list}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
